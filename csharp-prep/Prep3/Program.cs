a46741c21485b9cdd61558cdcc3c7b907dc2a780
bmcaldarella <bmcaldarella@gmail.com>
2024-01-07 16:34:48 +0000

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        do
+        {
+            PlayGame();
+
+            Console.WriteLine("Do you want to play again? (yes/no)");
+            string answer = Console.ReadLine().ToLower(); 
+
+            if (answer != "yes")
+            {
+                Console.WriteLine("Thanks for playing. Goodbye!");
+                break; 
+            }
+
+        } while (true);
+    }
+
+    static void PlayGame()
+    {
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int magicNumber;
+        int attempts = 0;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+
+            bool isValidInput = int.TryParse(userInput, out magicNumber);
+
+            if (!isValidInput)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                continue;
+            }
+
+            if (magicNumber > number)
+            {
+                Console.WriteLine("Lower");
+                attempts++;
+            }
+            else if (magicNumber < number)
+            {
+                Console.WriteLine("Higher");
+                attempts++;
+            }
+            else
+            {
+                Console.WriteLine("Congratulations!");
+                Console.WriteLine($"Your attempts: {attempts}");
+            }
+
+        } while (magicNumber != number);
     }
 }
